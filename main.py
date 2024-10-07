e8339ce91554585c690f1597850d82dd84a254b1
Emerson <emersonfilho953@gmail.com>
2024-10-07 11:01:36 -0300

    Atualização do arquivo main.py e outras melhorias

--- main.py
+++ main.py
@@ -145,7 +145,6 @@ def buscar_clientes():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
     # Configuração para o Cloud Run
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
